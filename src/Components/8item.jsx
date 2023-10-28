0ce3237d4e510a982bb650df12f977d1d92e2abb
abhinav2210103 <abhinav2210103@akgec.ac.in>
2023-10-28 15:37:09 +0530

    productpriceadded

--- src/Components/8item.jsx
+++ src/Components/8item.jsx
@@ -9,6 +9,7 @@ import metalbasket from '../assets/metalbasket.jpeg'
 import jutebag from '../assets/jute bag/mainjutebag.webp'
 import linentowel from '../assets/linentowel/maintowel.jpg'
 import largeglassbox from '../assets/largeglassbox/largeglassmain.jpg'
+import star from '../assets/stars.png'
 function Item() {
   return (
     <div className="item8">
@@ -17,21 +18,65 @@ function Item() {
         <img src={mat} className="image" style={{height:300, width:300}}/>
         <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
         <div className='searchbutton1'>VIEW ALL</div>
+        <div className="hoveritemtext">
+          <div className="productname">
+             Round Jute Placemat
+          </div>
+          <div className="stars">
+            <img src={star}/>
+          </div>
+          <div className="priceproduct">
+               30$
+          </div>
+        </div>
       </div>
       <div className="image-container">
         <img src={basket} className="image" style={{height:300, width:300}}/>
         <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
         <div className='searchbutton1'>VIEW ALL</div>
+        <div className="hoveritemtext">
+          <div className="productname">
+             Round Jute Placemat
+          </div>
+          <div className="stars">
+            <img src={star}/>
+          </div>
+          <div className="priceproduct">
+               30$
+          </div>
+        </div>
       </div>
       <div className="image-container">
         <img src={glassbox} className="image" style={{height:300, width:300}}/>
         <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
         <div className='searchbutton1'>VIEW ALL</div>
+        <div className="hoveritemtext">
+          <div className="productname">
+             Round Jute Placemat
+          </div>
+          <div className="stars">
+            <img src={star}/>
+          </div>
+          <div className="priceproduct">
+               30$
+          </div>
+        </div>
       </div>
       <div className="image-container">
       <img src={plate} className="image" style={{height:300, width:300}}/>
         <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
         <div className='searchbutton1'>VIEW ALL</div>
+        <div className="hoveritemtext">
+          <div className="productname">
+             Round Jute Placemat
+          </div>
+          <div className="stars">
+            <img src={star}/>
+          </div>
+          <div className="priceproduct">
+               30$
+          </div>
+        </div>
       </div>
       </div>
       <div className="lower4">
@@ -39,21 +84,65 @@ function Item() {
       <img src={metalbasket} className="image" style={{height:300, width:300}}/>
         <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
         <div className='searchbutton1'>VIEW ALL</div>
+        <div className="hoveritemtext">
+          <div className="productname">
+             Round Jute Placemat
+          </div>
+          <div className="stars">
+            <img src={star}/>
+          </div>
+          <div className="priceproduct">
+               30$
+          </div>
+        </div>
       </div>
       <div className="image-container">
       <img src={jutebag} className="image" style={{height:300, width:300}}/>
         <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
         <div className='searchbutton1'>VIEW ALL</div>
+        <div className="hoveritemtext">
+          <div className="productname">
+             Round Jute Placemat
+          </div>
+          <div className="stars">
+            <img src={star}/>
+          </div>
+          <div className="priceproduct">
+               30$
+          </div>
+        </div>
       </div>
       <div className="image-container">
       <img src={linentowel} className="image" style={{height:300, width:300}}/>
         <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
         <div className='searchbutton1'>VIEW ALL</div>
+        <div className="hoveritemtext">
+          <div className="productname">
+             Round Jute Placemat
+          </div>
+          <div className="stars">
+            <img src={star}/>
+          </div>
+          <div className="priceproduct">
+               30$
+          </div>
+        </div>
       </div>
       <div className="image-container">
       <img src={largeglassbox} className="image" style={{height:300, width:300}}/>
         <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
         <div className='searchbutton1'>VIEW ALL</div>
+        <div className="hoveritemtext">
+          <div className="productname">
+             Round Jute Placemat
+          </div>
+          <div className="stars">
+            <img src={star}/>
+          </div>
+          <div className="priceproduct">
+               30$
+          </div>
+        </div>
       </div>
       </div>
       </div>
