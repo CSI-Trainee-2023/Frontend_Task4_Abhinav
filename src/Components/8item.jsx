2507cf6f66df2af85f434b47b6679ba41858bea9
abhinav2210103 <abhinav2210103@akgec.ac.in>
2023-10-28 15:04:40 +0530

    hoveradded

--- src/Components/8item.jsx
+++ src/Components/8item.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../index.css'
+import Hover from 'hover'
 import mat from '../assets/mainmat.jpg'
 import basket from '../assets/mainbasket.webp'
 import glassbox from '../assets/mainglassbox.webp'
@@ -9,38 +10,53 @@ import jutebag from '../assets/jute bag/mainjutebag.webp'
 import linentowel from '../assets/linentowel/maintowel.jpg'
 import largeglassbox from '../assets/largeglassbox/largeglassmain.jpg'
 function Item() {
-  return(
-    <div className="8item">
-      <div className="upper8">
-        <div className="upper1">
-         <img src={mat} style={{ width: 300, height: 300 }}/>
-        </div>
-        <div className="upper2">
-         <img src={basket} style={{ width: 300, height: 300 }}/>
-        </div>
-        <div className="upper3">
-          <img src={glassbox} style={{ width: 300, height: 300 }}/>
-        </div>
-        <div className="upper4">
-         <img src={plate} style={{ width: 300, height: 300 }}/>
-        </div>
+  return (
+    <div className="item8">
+      <div className='upper4'>
+      <div className="image-container">
+        <img src={mat} className="image" style={{height:300, width:300}}/>
+        <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        <div className='searchbutton1'>VIEW ALL</div>
       </div>
-      <div className="lower8">
-         <div className="lower1">
-         <img src={metalbasket} style={{ width: 300, height: 300 }}/>
-         </div>
-         <div className="lower2">
-         <img src={jutebag} style={{ width: 300, height: 300 }}/>
-         </div>
-         <div className="lower3">
-         <img src={linentowel} style={{ width: 300, height: 300 }}/>
-         </div>
-         <div className="lower4">
-         <img src={largeglassbox} style={{ width: 300, height: 300 }}/>
-         </div>
+      <div className="image-container">
+        <img src={basket} className="image" style={{height:300, width:300}}/>
+        <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        <div className='searchbutton1'>VIEW ALL</div>
       </div>
-    </div>
-  )
+      <div className="image-container">
+        <img src={glassbox} className="image" style={{height:300, width:300}}/>
+        <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        <div className='searchbutton1'>VIEW ALL</div>
+      </div>
+      <div className="image-container">
+      <img src={plate} className="image" style={{height:300, width:300}}/>
+        <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        <div className='searchbutton1'>VIEW ALL</div>
+      </div>
+      </div>
+      <div className="lower4">
+      <div className="image-container">
+      <img src={metalbasket} className="image" style={{height:300, width:300}}/>
+        <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        <div className='searchbutton1'>VIEW ALL</div>
+      </div>
+      <div className="image-container">
+      <img src={jutebag} className="image" style={{height:300, width:300}}/>
+        <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        <div className='searchbutton1'>VIEW ALL</div>
+      </div>
+      <div className="image-container">
+      <img src={linentowel} className="image" style={{height:300, width:300}}/>
+        <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        <div className='searchbutton1'>VIEW ALL</div>
+      </div>
+      <div className="image-container">
+      <img src={largeglassbox} className="image" style={{height:300, width:300}}/>
+        <div className="hover-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+        <div className='searchbutton1'>VIEW ALL</div>
+      </div>
+      </div>
+      </div>
+    );
 }
-
 export default Item
